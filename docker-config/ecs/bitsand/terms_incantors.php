ace28ae8511835ffd3687a640ce370e543dc22a8
Jonathan Harden <jfharden@gmail.com>
2018-09-15 17:07:46 +0100

    Add terms for incantors

---
+++ docker-config/ecs/bitsand/terms_incantors.php
@@ -0,0 +1,66 @@
+<?php
+/*-----------------------------------------------------------------------------
+ | Bitsand - an online booking system for Live Role Play events
+ |
+ | File terms_dist.php
+ |     Author: Russell Phillips
+ |  Copyright: (C) 2006 - 2015 The Bitsand Project
+ |             (http://github.com/PeteAUK/bitsand)
+ |
+ | Bitsand is free software; you can redistribute it and/or modify it under the
+ | terms of the GNU General Public License as published by the Free Software
+ | Foundation, either version 3 of the License, or (at your option) any later
+ | version.
+ |
+ | Bitsand is distributed in the hope that it will be useful, but WITHOUT ANY
+ | WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ | FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ | details.
+ |
+ | You should have received a copy of the GNU General Public License along with
+ | Bitsand.  If not, see <http://www.gnu.org/licenses/>.
+ +---------------------------------------------------------------------------*/
+
+//Do not need login check for this page
+$bLoginCheck = False;
+
+include ('inc/inc_head_db.php');
+include ('inc/inc_head_html.php');
+?>
+<h1>Scheming Demon - Terms &amp; Conditions, Rules &amp Regulations</h1>
+
+<ol>
+    <li style='margin-bottom: 0.5em'>
+This event will be using the Lorien LRP Rule system V3. The Lorien LRP System V3 is copyright to the Lorien Trust. The Lorien Trust bears no responsibility for anything occurring at this event.
+    </li>
+    <li style='margin-bottom: 0.5em'>
+LRP is a potentially dangerous hobby and I agree to act in a reasonable and safe manner during any event run by Scheming Demon Events.
+    </li>
+    <li style='margin-bottom: 0.5em'>
+The event organisers and staff cannot be personally liable or held responsible for any injury or damage to any individual participating in any Scheming Demon Events event.
+    </li>
+    <li style='margin-bottom: 0.5em'>
+The event organisers cannot take any responsibility for the loss of property or theft of any personal or communal property during any Scheming Demon Events event.
+    </li>
+    <li style='margin-bottom: 0.5em'>
+The event organisers reserve the right to refuse admission or participation to their events without explanation.
+    </li>
+    <li style='margin-bottom: 0.5em'>
+Metal Weapons or Tools are not to be carried onto site. Camping Knives may only be carried in direct proximity to designated camping areas
+    </li>
+    <li style='margin-bottom: 0.5em'>
+The organisers will expel any individual engaged in illegal, defamatory or dangerous activities during the event.
+    </li>
+    <li style='margin-bottom: 0.5em'>
+The referees' decision is final in all disputes.
+    </li>
+    <li style='margin-bottom: 0.5em'>
+Any cancellations received up to 14 days prior to the event will be subject to a &pound;10 administration fee. Any cancellation received after this deadline will NOT be refunded.
+    </li>
+    <li style='margin-bottom: 0.5em'>
+I understand that LRP is a contact hobby and by signing this form I agree to take part in safe combat and be struck by weapons checked by LT approved weapons checkers.
+    </li>
+</ol>
+
+<?php
+include ('inc/inc_foot.php');
