d7ecb7b3eff9b2f7004eacc61490a6d81d063b49
Jonathan Harden <jfharden@gmail.com>
2019-08-28 12:39:52 +0100

    Add jackals terms

---
+++ docker-config/ecs/bitsand/terms_jackals.php
@@ -0,0 +1,96 @@
+<?php
+/*-----------------------------------------------------------------------------
+ | Bitsand - an online booking system for Live Role Play events
+ |
+ | File terms_dist.php
+ |     Author: Russell Phillips
+ |  Copyright: (C) 2006 - 2015 The Bitsand Project
+ |             (http://github.com/PeteAUK/bitsand)
+ |
+ | Bitsand is free software; you can redistribute it and/or modify it under the
+ | terms of the GNU General Public License as published by the Free Software
+ | Foundation, either version 3 of the License, or (at your option) any later
+ | version.
+ |
+ | Bitsand is distributed in the hope that it will be useful, but WITHOUT ANY
+ | WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ | FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ | details.
+ |
+ | You should have received a copy of the GNU General Public License along with
+ | Bitsand.  If not, see <http://www.gnu.org/licenses/>.
+ +---------------------------------------------------------------------------*/
+
+//Do not need login check for this page
+$bLoginCheck = False;
+
+include ('inc/inc_head_db.php');
+include ('inc/inc_head_html.php');
+?>
+<h1><?php echo TITLE?> - Terms &amp; Conditions LOCAL INSTALL</h1>
+
+
+<h3>Event &amp; Web Site Terms &amp; Regulations</h3>
+
+<p>
+Please note that fuller rules and regulations and notes on particular issues will be mentioned in the event guide.
+</p>
+
+<h4>By attending this event, you acknowledge that</h4>
+
+<p>
+The organisers' decision is final in any/all disputes. 
+</p>
+
+<p>
+The organisers reserve the right to bar entry to, or expel, any person considered undesirable to the good name of LRP, the organisers, or The Lorien Trust. The organisers are not obliged to give any explanation for their decision. 
+</p>
+
+<p>
+The organisers will inform the authorities if anyone attending the event is caught breaking the law. 
+</p>
+
+<p>
+The organisers will expel anyone found engaging in activities potentially dangerous to themselves or others, or whose activities are considered to be disruptive to the event, the good name of LRP, the organisers, or The Lorien Trust. 
+</p>
+
+<p>
+The organisers will charge you for the repair or replacement of any of the organisers' property (or property in the care of the organisers) that you have lost, damaged, vandalised, destroyed or stolen at any of the organisers' events. 
+</p>
+
+<p>
+Refunds on applications will be subject to an administration fee. Cancellations made with seven days of the start of the event will not be refunded. Tickets are non-transferable, except by prior arrangement with the organisers. 
+</p>
+
+<p>
+Secondary characters may only be played by prior arrangement with the organisers. 
+</p>
+
+<p>
+The organisers request that all potentially serious medical conditions be declared when booking, and that any changes to medical conditions and/or next-of-kin details be declared to the organisers in writing as soon as they become known. Failure to do so is at the attendee's own risk. 
+</p>
+
+<p>
+All participants of a Live Role Play event agree that LRP safe weapons may be used on them. 
+</p>
+
+<h4>By using this website, you acknowledge that</h4>
+
+<p>
+Your details will be stored and used for the purposes of booking and administering events run by the Jackal Faction, and will not be shared with third parties without your consent.
+</p>
+
+<p>
+All access to this website will be logged. The primary purpose of these logs is to help discover, and recover from, malicious attacks and attempts to glean personal information.
+</p>
+
+<p>
+You will be required to sign a declaration of agreement to the Jackal Faction's, Sanctioned Events and the Site Custiodian's regulations upon arrival at any events booked through this system. You will not be allowed to participate in the event until said declaration has been signed. If you attempt to participate in the event without signing the declaration, you may be ejected from the site.
+</p>
+
+<p>
+Keeping your in character and real world information up to date is your responsibility, and the Jackal Faction cannot be held responsible for errors made due to out of date information.
+</p>
+
+<?php
+include ('inc/inc_foot.php');
