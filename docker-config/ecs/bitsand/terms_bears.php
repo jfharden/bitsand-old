2d1ebdc8ada4f8ee60dc0fac384ebf795790d33b
Jonathan Harden <jfharden@gmail.com>
2019-10-29 20:36:25 +0000

    Add correct bears T and Cs

--- docker-config/ecs/bitsand/terms_bears.php
+++ docker-config/ecs/bitsand/terms_bears.php
@@ -27,57 +27,38 @@ $bLoginCheck = False;
 include ('inc/inc_head_db.php');
 include ('inc/inc_head_html.php');
 ?>
-<h1><?php echo TITLE?> - Terms &amp; Conditions LOCAL INSTALL</h1>
-
-<!--
-Provide full terms and conditions below. The text here is from the Lions (Lorien
-Trust) booking site, and can be used as an example/template
--->
-
-<h3>Event &amp; Web Site Terms &amp; Regulations</h3>
+<h1><?php echo TITLE?> - Terms &amp; Conditions</h1>
 
 <p>
-Please note that fuller rules and regulations and notes on particular issues will be mentioned in the event guide and on the <a href = "http://www.lionsfaction.co.uk/">Lions website</a>
+By attending this event you acknowledge and accept the following Rules & Regulations as well as Booking Terms & Conditions
 </p>
 
-<h4>By attending this event, you acknowledge that</h4>
+<h2>
+Rules and Regulations:
+</h2>
 
 <p>
-To utilise a special effect or power, you must have the appropriate in game prop and authorisation. Or, to put it another way - No Phys Rep, No Effect and No Card, No Effect. Only LRP safe weapons will be allowed on site, subject to checking at registration. Any items judged unsafe should be returned to your vehicle, or can be stored by Games Ops for you during the course of the event. Any noncombat props should be announced on your booking form, presented for a safety check on registration and clearly marked as an NCP throughout the event.
+This is a summary of the main rules. Full rules can be found at <a href="http://www.bearsfaction.org.uk/forum">http://www.bearsfaction.org.uk/forum</a> or <a href="http://www.bearsfaction.org.uk/forum">https://www.facebook.com/bearsfaction</a>
 </p>
 
 <p>
-Burning Ambitions, Sanctioned Events and the Site Custodian cannot be held responsible for the loss or theft of OOC valuables. Secure them in your vehicle, or better yet, don't bring them at all. All lost property should be handed in at Games Ops which is where you should direct any enquiries.
+The organisers and their staff accept no responsibility for injury to you or loss or damage to your property, unless due to negligence on the part of the organisers. The organisers’ decision is final in any/all disputes. The organisers reserve the right to ask you to leave the event at any time, at their discretion. All participants agree that LRP useable weapons may be used on them. Laird of Caledonia Events use the Lorien Trust LRP Version 3 Rules System. This system is Copyright to the Lorien Trust. Event specific rules will be in the event guide and must be complied with.. All bookings are subject to Laird of Caledonia Booking Terms & Conditions (see below). You will be required to sign a declaration of agreement to Lairds of Caledonia and the Site Custiodian's regulations upon arrival at any events booked through this system. You will not be allowed to participate in the event until said declaration has been signed. If you attempt to participate in the event without signing the declaration, you may be ejected from the site.
+</p>
+
+<h2>
+Booking Terms & Conditions:
+</h2>
+
+<p>
+Bookings: Acceptance of application is at the discretion of the organisers. All bookings are non-transferable. In the event of character death prior to or inbetween Lairds of Caledonia events, players booked are required to contact the event organisers to confirm a new character spend no less than 1 week before the next Lairds of Caledonia event for which the player has made a booking. Event ticket pricing includes camping fees.
 </p>
 
 <p>
-No controlled substances other than legitimately prescribed drugs will be tolerated on site. Anyone discovered to be in possession of such will be ejected and the relevant authorities notified.
+Refund Policy: Event ticket refunds will only be given if cancellation is more than 14 days before the start of the event and will be subject to a £10 admin fee. Should the event organisers decide to cancel an event a full refund will be provided for any bookings made.
 </p>
 
 <p>
-Burning Ambitions and Sanctioned Events reserve the right to eject anyone from the event for any breach of the regulations of either Burning Ambitions, Sanctioned Events or the site custodian.
-</p>
-
-<p>
-Only referees nominated by the event organisers may direct any kind of plot at this event and any such referee's decision is final. Nominated referees will be carrying Burning Ambitions ref cards.
-</p>
-
-<h4>By using this website, you acknowledge that</h4>
-
-<p>
-Your details will be stored and used for the purposes of booking and administering events run by Burning Ambitions, and will not be shared with third parties without your consent.
-</p>
-
-<p>
-All access to this website will be logged. The primary purpose of these logs is to help discover, and recover from, malicious attacks and attempts to glean personal information.
-</p>
-
-<p>
-You will be required to sign a declaration of agreement to Burning Ambitions, Sanctioned Events and the Site Custiodian's regulations upon arrival at any events booked through this system. You will not be allowed to participate in the event until said declaration has been signed. If you attempt to participate in the event without signing the declaration, you may be ejected from the site.
-</p>
-
-<p>
-Keeping your in character and real world information up to date is your responsibility, and Burning Ambitions cannot be held responsible for errors made due to out of date information.
+By using this website, you acknowledge that: Your details will be stored and used for the purposes of booking and administering events run by Lairds of Caledonia and will not be shared with third parties without your consent. All access to this website will be logged. The primary purpose of these logs is to help discover, and recover from, malicious attacks and attempts to glean personal information. Keeping your in character and real world information up to date is your responsibility, and Lairds of Caledonia cannot be held responsible for errors made due to out of date information.
 </p>
 
 <?php
