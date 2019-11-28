233ac79202715b84e655e942fc68c92d946982f2
Jonathan Harden <jfharden@gmail.com>
2019-11-28 19:48:15 +0000

    Add Wolves terms

---
+++ docker-config/ecs/bitsand/terms_wolves.php
@@ -0,0 +1,109 @@
+<rts
+?php
+/*
+Bitsand - a web-based booking system for LRP events
+Copyright (C) 2006 - 2012 The Bitsand Project (http://bitsand.googlecode.com/)
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+//Do not need login check for this page
+$bLoginCheck = False;
+
+include ('inc/inc_head_db.php');
+include ('inc/inc_head_html.php');
+?>
+<h1><?php echo TITLE?> - Terms &amp; Conditions</h1>
+
+<!--
+Provide full terms and conditions below. The text here is from the Lions (Lorien
+Trust) booking site, and can be used as an example/template
+-->
+
+<h3>Event &amp; Web Site Terms &amp; Regulations</h3>
+
+<p>Rules and Regulations
+</p><p>
+
+1. Wolves Faction LRP or any persons so appointed by
+
+Wolves Faction LRP cannot be held personally liable for
+
+any injury or damage to any individual participating in
+
+Wolves Faction LRP event.
+</p><p>
+2. Wolves Faction LRP cannot be held responsible for any
+
+damage to, loss, or theft of any personal or communal
+
+property during any event.
+</p><p>
+3. LRP is a potentially dangerous hobby and I agree to act
+
+in a reasonable and safe manner during any LRP event.
+</p><p>
+4. I understand and agree that Wolves Faction LRP
+
+reserves the right to refuse admission or participation in
+
+any event or part of an event to any individual at any
+
+time. The organisers are not obliged to give any reason
+
+or explanation for such a decision, but one may be
+
+supplied after a request in writing.
+</p><p>
+5. Metal weapons or tools are NOT to be carried onto site.
+
+6. The organisers will expel any individual engaged in
+
+illegal, defamatory, or dangerous practices during the
+
+7. The Wolves Faction operates a strict ‘NO CARD, NO
+
+EFFECT’ policy. Any and all special items, potions etc.
+
+must be disclosed prior to the event. There are no
+
+exceptions to this rule.
+</p><p>
+8. The referees’ decision is final in all disputes.
+</p><p>
+9. Any cancellations received up to ten days prior to the
+
+event will be subject to a £10 administration fee. Any
+
+cancellation received after this deadline will NOT be
+
+refunded.
+</p><p>
+10. The Lorien Trust is not responsible for any incidents
+
+at this event.
+</p><p>
+11. I understand that LRP is a contact hobby and by
+
+signing this form I agree to take part in safe combat and
+
+be struck by weapons checked by LT approved weapons
+
+checkers.
+</p>
+
+<?php
+include ('inc/inc_foot.php');
+?>
